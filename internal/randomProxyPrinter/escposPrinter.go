59c9430bdfa2f402e42619602e8a7ffcef6b43ce
Justin Hendrickson <justin.hendrickson@gmail.com>
2021-11-12 15:08:18 -0600

    Refactore escpos library

--- internal/randomProxyPrinter/escposPrinter.go
+++ internal/randomProxyPrinter/escposPrinter.go
@@ -5,34 +5,32 @@ import (
 	_ "image/png"
 	"strings"
 
-	"github.com/cloudinn/escpos"
+	"github.com/kenshaw/escpos"
 	"github.com/mitchellh/go-wordwrap"
 )
 
 type ESCPOSPrinter struct {
-	escpos *escpos.Printer
+	escpos *escpos.Escpos
 }
 
-func NewESCPOSPrinter(escpos *escpos.Printer) *ESCPOSPrinter {
+func NewESCPOSPrinter(escpos *escpos.Escpos) *ESCPOSPrinter {
 	return &ESCPOSPrinter{escpos: escpos}
 }
 
 func (t *ESCPOSPrinter) Print(proxy Proxy) error {
-	if _, err := t.escpos.Write(proxy.PrintData); err != nil {
+	if _, err := t.escpos.WriteRaw(proxy.PrintData); err != nil {
 		return fmt.Errorf("write print data: %w", err)
 	}
 
 	for _, line := range strings.Split(proxy.Description, "\n") {
 		for _, wrappedLine := range strings.Split(wordwrap.WrapString(line, 32), "\n") {
-			if err := t.escpos.Text(map[string]string{}, wrappedLine); err != nil {
-				return fmt.Errorf("write text: %w", err)
-			}
+			t.escpos.Text(map[string]string{}, wrappedLine)
+			t.escpos.Feed(map[string]string{})
 		}
+		t.escpos.Feed(map[string]string{})
 	}
 
-	_ = t.escpos.Feed(map[string]string{})
-	_ = t.escpos.Feed(map[string]string{})
-	_ = t.escpos.Feed(map[string]string{})
+	t.escpos.Feed(map[string]string{"line": "2"})
 
 	return nil
 }
